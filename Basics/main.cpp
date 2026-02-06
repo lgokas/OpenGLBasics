69efcb999cf756f6e3d5ff5b9a153e713077be9c
lgokas <lukasgokas94@gmail.com>
2026-02-06 10:43:14 +0000

    Latest terrain updates

--- Basics/main.cpp
+++ Basics/main.cpp
@@ -1,4 +1,4 @@
-#include "glad/glad.h"
+﻿#include "glad/glad.h"
 #include <GLFW/glfw3.h>
 #include "shaders.h"
 #include <iostream>
@@ -20,7 +20,7 @@ const unsigned int SCR_WIDTH = 1600;
 const unsigned int SCR_HEIGHT = 1200;
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 120.0f, 120.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -37,8 +37,6 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-
 
 	// glfw window creation
 	// --------------------
@@ -65,40 +63,155 @@ int main()
 	}
 
 	glEnable(GL_DEPTH_TEST);
+	stbi_set_flip_vertically_on_load(true);
+
+	// load height map texture
+	int width, height, nChannels;
+	unsigned char* data = stbi_load("../external/basic_heightmap.png", &width, &height, &nChannels, 0);
+	if (data)
+	{
+		std::cout << "Loaded heightmap of size " << height << " x " << width << std::endl;
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	// load height map texture
+	int colourmapwidth, colourmapheight, colourmapnChannels;
+	unsigned char* colourmapdata = stbi_load("../external/basic_colourmap.png", &colourmapwidth, &colourmapheight, &colourmapnChannels, 0);
+	if (colourmapdata)
+	{
+		std::cout << "Loaded colourmap of size " << colourmapwidth << " x " << colourmapheight << std::endl;
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	std::vector<float> vertices;
+	float yScale = 64.0f / 256.0f, yShift = 16.0f;
+	int rez = 1;
+	unsigned bytePerPixel = nChannels;
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			unsigned char* pixelOffset = data + (j + width * i) * bytePerPixel;
+			unsigned char y = pixelOffset[0];
+
+			float vx = (float)j - width / 2.0f;
+			float vz = (float)i - height / 2.0f;
+			float vy = (int)y * yScale - yShift;
+
+			// IMPORTANT: UVs must match grid layout EXACTLY
+			float u = j / (float)(width - 1);
+			float v = i / (float)(height - 1);
+
+			vertices.push_back(vx);
+			vertices.push_back(vy);
+			vertices.push_back(vz);
+			vertices.push_back(u);
+			vertices.push_back(v);
+		}
+	}
+
+
+	std::cout << "Loaded " << vertices.size() / 5 << " vertices" << std::endl;
+	stbi_image_free(data);
+
+	std::vector<unsigned> indices;
+	for (unsigned i = 0; i < height - 1; i += rez)
+	{
+		for (unsigned j = 0; j < width; j += rez)
+		{
+			for (unsigned k = 0; k < 2; k++)
+			{
+				indices.push_back(j + width * (i + k * rez));
+			}
+		}
+	}
+	std::cout << "Loaded " << indices.size() << " indices" << std::endl;
+
+	const int numStrips = (height - 1) / rez;
+	const int numTrisPerStrip = (width / rez) * 2 - 2;
+	std::cout << "Created lattice of " << numStrips << " strips with " << numTrisPerStrip << " triangles each" << std::endl;
+	std::cout << "Created " << numStrips * numTrisPerStrip << " triangles total" << std::endl;
+
+	// first, configure the cube's VAO (and terrainVBO + terrainIBO)
+	unsigned int terrainVAO, terrainVBO, terrainIBO;
+	glGenVertexArrays(1, &terrainVAO);
+	glBindVertexArray(terrainVAO);
+
+	glGenBuffers(1, &terrainVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, terrainVBO);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
+
+	// position
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// texture coordinates
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+
+	glGenBuffers(1, &terrainIBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, terrainIBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned), &indices[0], GL_STATIC_DRAW);
+
+	// Texture stuff
+	unsigned int colourTexture;
+	glGenTextures(1, &colourTexture);
+	glBindTexture(GL_TEXTURE_2D, colourTexture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	GLenum dataFormat;
+	GLenum internalFormat;
+
+	if (colourmapnChannels == 1)
+	{
+		internalFormat = GL_R8;
+		dataFormat = GL_RED;
+	}
+	else if (colourmapnChannels == 3)
+	{
+		internalFormat = GL_RGB8;
+		dataFormat = GL_RGB;
+	}
+	else if (colourmapnChannels == 4)
+	{
+		internalFormat = GL_RGBA8;
+		dataFormat = GL_RGBA;
+	}
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);   // ⭐ CRITICAL
+
+	glTexImage2D(GL_TEXTURE_2D,
+		0,
+		internalFormat,
+		colourmapwidth,
+		colourmapheight,
+		0,
+		dataFormat,
+		GL_UNSIGNED_BYTE,
+		colourmapdata);
+
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(colourmapdata);
+
 
 	// build and compile our shader program
 	// ------------------------------------
 	Shader shader("../external/VertexShader.txt", "../external/FragmentShader.txt"); // you can name your shader files however you like
-
-	// set up vertex data (and buffer(s)) and configure vertex attributes
-	// ------------------------------------------------------------------
-	float vertices[] = {
-		// positions         // colors
-		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
-	};
-
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	// color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-
-	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-	// glBindVertexArray(0);
-
 
 	// render loop
 	// -----------
@@ -116,30 +229,37 @@ int main()
 
 		// render
 		// ------
-		glClearColor(0.0f, 0.0f, 0.5f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// render the triangle
 		shader.use();
 
 		// pass projection matrix to shader (note that in this case it could change every frame)
-		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100000.0f);
 		shader.setMat4("projection", projection);
 
 		// camera/view transformation
 		glm::mat4 view = camera.GetViewMatrix();
 		shader.setMat4("view", view);
 
+		glm::mat4 model = glm::mat4(1.0f);
+		shader.setMat4("model", model);
 
-		glm::mat4 triangleModel = glm::mat4(1.0f);
-		triangleModel = glm::translate(triangleModel, glm::vec3(0.0f, 0.0f, -3.0f));
-		triangleModel = glm::scale(triangleModel, glm::vec3(1.0f, 1.0f, 1.0f));
-		triangleModel = glm::rotate(triangleModel, glm::radians(0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-		shader.setMat4("model", triangleModel);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, colourTexture);
 
+		shader.setInt("colourMap", 0);
 
+		glBindVertexArray(terrainVAO);
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		for (unsigned strip = 0; strip < numStrips; strip++)
+		{
+			glDrawElements(GL_TRIANGLE_STRIP,   // primitive type
+				numTrisPerStrip + 2,   // number of indices to render
+				GL_UNSIGNED_INT,     // index data type
+				(void*)(sizeof(unsigned) * (numTrisPerStrip + 2) * strip)); // offset to starting index
+		}
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
@@ -149,8 +269,8 @@ int main()
 
 	// optional: de-allocate all resources once they've outlived their purpose:
 	// ------------------------------------------------------------------------
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	//glDeleteVertexArrays(1, &VAO);
+	//glDeleteBuffers(1, &VBO);
 
 	// glfw: terminate, clearing all previously allocated GLFW resources.
 	// ------------------------------------------------------------------
